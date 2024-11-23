54fcdbe74a453a9a9501fc500bdd61cb1d5a35b2
PaulLupse <Lupse.Io.Paul@student.utcluj.ro>
2024-11-23 11:24:23 +0200

    Added Sorts to Utilities

--- Half-baked Apps/Thingy/Main.py
+++ Half-baked Apps/Thingy/Main.py
@@ -5,11 +5,4 @@ from IPython.core.magics.execution import Timer
 import Utilities as Utils
 
 Timer1 = Utils.Timer()
-Timer1.pause_timer()
-Timer1.start_timer()
-Timer1.resume_timer()
-while Timer1.running is not True:
-    pass
-time.sleep(5.5)
-Timer1.stop_timer()
-print(round(Timer1.time_passed, 3))
+
--- Half-baked Apps/Thingy/Utilities.py
+++ Half-baked Apps/Thingy/Utilities.py
@@ -1,5 +1,6 @@
 import threading as thrd
 import time
+import numpy as np
 
 class CDTimer:
     def __init__(self, period):
@@ -9,7 +10,7 @@ class CDTimer:
         self.__pause = False
         self.__stop = False
 
-    def __timing(self):
+    def __run(self):
         while self.__pperiod > 0 and self.__stop is not True:
             if self.__pause is not True:
                 self.__pperiod -= 0.01
@@ -20,7 +21,7 @@ class CDTimer:
         return
 
     def start_timer(self):
-        thrd.Thread(target = self.__timing).start()
+        thrd.Thread(target = self.__run).start()
         self.running = True
 
     def pause_timer(self):
@@ -29,39 +30,6 @@ class CDTimer:
 
     def resume_timer(self):
         self.__pause = False
-        self.running = True
-
-    def stop_timer(self):
-        self.__stop = True
-        self.running = False
-
-class Timer:
-    def __init__(self):
-        self.time_passed = 0
-        self.running = False
-        self.__stop = False
-        self.__run = False
-
-    def __timing(self):
-        self.running = True
-        while True:
-            if self.__stop is True:
-                break
-            if self.__run is True:
-                self.time_passed += 0.01
-                time.sleep(0.01)
-        return
-
-    def start_timer(self):
-        self.__run = True
-        thrd.Thread(target = self.__timing).start()
-
-    def pause_timer(self):
-        self.__run = False
-        self.running = False
-
-    def resume_timer(self):
-        self.__run = True
         self.running = True
 
     def stop_timer(self):
@@ -88,15 +56,108 @@ class StopWatch:
         self.__stop_time = time.time()
         self.time_passed += self.__stop_time - self.__start_time
 
-if __name__ == "__main__":
-    Timer1 = StopWatch()
-    Timer1.start_timer()
-    time.sleep(1.234)
-    Timer1.pause_timer()
-    time.sleep(2)
-    Timer1.resume_timer()
-    time.sleep(3.141)
-    Timer1.stop_timer()
-    print(Timer1.time_passed)
+class Sort:
+    def BubbleSort(self, arr):
+        arrL = len(arr)
+        sorted = False
+        j = 0
+        while sorted is False:
+            sorted = True
+            for i in range(0, arrL - 1 - j):
+                if arr[i] > arr[i + 1]:
+                    sorted = False
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            j += 1
 
+    def StupidSort(self, arr):
+        arrL = len(arr)
+        for i in range(0, arrL - 1):
+            for j in range(i + 1, arrL):
+                if arr[i] > arr[j]:
+                    arr[i], arr[j] = arr[j], arr[i]
+
+    def SelectionSort(self, arr):
+        arrL = len(arr)
+        for i in range(0, arrL - 1):
+            imin = i
+            for j in range(i + 1, arrL):
+                if arr[j] < arr[imin]:
+                    imin = j
+
+            arr[imin], arr[i] = arr[i], arr[imin]
+
+    def InsertionSort(self, arr):
+        arrL = len(arr)
+        arr2 = np.zeros((arrL,), np.int64)
+        k = 0
+        for number in arr:
+            i = k
+            while arr2[i-1] > number and i-1 >= 0:
+                arr2[i] = arr2[i-1]
+                i -= 1
+            arr2[i] = number
+            k += 1
+
+        for i in range(0, arrL):
+            arr[i] = arr2[i]
+
+    def MergeSort(self, arr, left, right):
+        if left + 1 < right:
+            p = (left + right )//2
+            self.MergeSort(arr, left, p)
+            self.MergeSort(arr, p, right)
+            InterclassedArr = Interclass_np(arr[left:p], arr[p:right])
+            k = 0
+            for i in range(left, right):
+                arr[i] = InterclassedArr[k]
+                k += 1
+
+    def QuickSort(self, arr, left, right):
+        pass
+
+
+
+
+
+
+def Interclass_np(arr1, arr2):
+    i = 0; arr1L = len(arr1)
+    j = 0; arr2L = len(arr2)
+    k = 0
+
+    arrk = np.zeros((arr1L + arr2L,), np.int64)
+
+    while i < arr1L and j < arr2L:
+        if arr1[i] < arr2[j]:
+            arrk[k] = arr1[i]
+            i += 1
+        else:
+            arrk[k] = arr2[j]
+            j += 1
+        k += 1
+
+    while i < arr1L:
+        arrk[k] = arr1[i]
+        i += 1
+        k += 1
+    while j < arr2L:
+        arrk[k] = arr2[j]
+        j += 1
+        k += 1
+
+    return arrk
+
+
+if __name__ == "__main__":
+    a = np.array([1, 6, 5, 7, 1, 9])
+    b = np.array([9, 4, 2, 10, 1])
+
+    s = Sort()
+
+    s.MergeSort(a, 0, len(a))
+    s.MergeSort(b, 0, len(b))
+
+    print(a, b)
+
+    print(Interclass_np(a, b))
 
